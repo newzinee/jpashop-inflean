511caef950324b64ef558da1ae458797939941ce
newzin <qvo7896@gmail.com>
2020-02-21 00:52:55 +0900

    OSIV

---
+++ src/main/java/jpabook/jpashop/service/query/OrderQueryService.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.service.query;
+
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OrderQueryService {
+
+    private final OrderRepository orderRepository;
+}
+
